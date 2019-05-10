cc5c33da06058a179d95a74f9ff8696c4e032cb7
Jakub Dubovsky <jakub.dubovsky@seznam.cz>
2019-05-10 14:48:26 +0200

    test code

--- src/main/scala/com/github/james64/pustee/ReplProcess.scala
+++ src/main/scala/com/github/james64/pustee/ReplProcess.scala
@@ -4,6 +4,7 @@ import java.io._
 import java.lang.ProcessBuilder.Redirect
 
 import com.github.james64.pustee.TakeUntilWord.{NoWord, Taken}
+import com.pty4j.PtyProcess
 
 trait ReplProcess {
 
@@ -74,5 +75,41 @@ object ReplProcess {
     }
 
     override def destroy(): Unit = proc.destroyForcibly()
+  }
+}
+
+object Testtt {
+
+  def main(args: Array[String]): Unit = {
+    val pty = PtyProcess.exec(Array("/usr/bin/scala", "-Dscala.repl.prompt='adfadf'"))
+
+    val cmdOutputReader = new InputStreamReader(new BufferedInputStream(pty.getInputStream))
+    val cmdOutputIter = Iterator.continually(cmdOutputReader.read.toChar)
+    val promptWaiter = TakeUntilWord("adfadf")
+    val writer = new OutputStreamWriter(pty.getOutputStream)
+
+    val code =
+      """:paste
+        |val a = 1
+        |val b = 2
+        |a+b
+        |\u0004
+      """.stripMargin
+
+    println("one")
+    println(promptWaiter.take(cmdOutputIter))
+
+    println("two")
+    writer.write(code)
+    writer.flush()
+
+
+    // todo code (wrapped by scala repl's comments) is printed back to output stream by pty. We need to skip it.
+    println("three")
+    println(promptWaiter.take(cmdOutputIter))
+
+    println("last")
+
+    pty.destroy()
   }
 }
